d1176b3eb4c07381082a65ccbaf09a671e12ebd2
Matthew Nickson <mnickson@sidingsmedia.com>
2022-10-04 21:45:35 +0100

    Initial generation docs from create-openapi-repo
    Signed-off-by: Matthew Nickson <mnickson@sidingsmedia.com>

---
+++ openapi/code_samples/C#/echo/post.cs
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2016 Ivan Goncharov
+// SPDX-License-Identifier: MIT
+
+API.v1.Echo echo = new API.v1.Echo();
+echo.message = "Hello World!");
+EchoResponse response = echo.post();
+if (response.statusCode == HttpStatusCode.Created)
+{
+  // Success
+}
+else
+{
+  // Something wrong -- check response for errors
+  Console.WriteLine(response.getRawResponse());
+}
